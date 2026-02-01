5d2988d9ef5e71130b3827adec9f33898e144f4c
Szabolcs Horvath <szaby0927@gmail.com>
2026-02-01 18:23:46 +0100

    Implement caching and refactor logic in InstallationIdentifier
    - Add `cachedInstallationIdentifier` to reduce DataStore reads
    - Refactor identifier retrieval and generation into separate functions
    - Improve logging for identifier lifecycle events

--- app/src/main/java/com/szabolcshorvath/memorymap/util/InstallationIdentifier.kt
+++ app/src/main/java/com/szabolcshorvath/memorymap/util/InstallationIdentifier.kt
@@ -7,22 +7,38 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import com.szabolcshorvath.memorymap.dataStore
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
+import java.util.UUID
 
 object InstallationIdentifier {
     private const val TAG = "InstallationIdentifier"
     private val INSTALLATION_IDENTIFIER_KEY = stringPreferencesKey("installation_identifier")
 
+    private var cachedInstallationIdentifier: String? = null
+
     suspend fun getInstallationIdentifier(context: Context): String {
-        val installationIdentifier =
+        return cachedInstallationIdentifier ?: retrieveInstallationIdentifier(context)
+    }
+
+    private suspend fun retrieveInstallationIdentifier(context: Context): String {
+        val idFromDataStore =
             context.dataStore.data.map { it[INSTALLATION_IDENTIFIER_KEY] }.firstOrNull()
-        if (installationIdentifier == null) {
-            val newInstallationIdentifier = java.util.UUID.randomUUID().toString()
-            context.dataStore.edit { it[INSTALLATION_IDENTIFIER_KEY] = newInstallationIdentifier }
-            Log.i(TAG, "Generated new installation identifier: $newInstallationIdentifier")
-            return newInstallationIdentifier
+
+        if (idFromDataStore != null) {
+            Log.i(TAG, "Using existing installation identifier in dataStore: $idFromDataStore")
+            cachedInstallationIdentifier = idFromDataStore
+            return idFromDataStore
         } else {
-            Log.d(TAG, "Using existing installation identifier: $installationIdentifier")
-            return installationIdentifier
+            return generateInstallationIdentifier(context)
         }
+    }
+
+    private suspend fun generateInstallationIdentifier(context: Context): String {
+        val newInstallationIdentifier = UUID.randomUUID().toString()
+        context.dataStore.edit {
+            it[INSTALLATION_IDENTIFIER_KEY] = newInstallationIdentifier
+        }
+        Log.i(TAG, "Generated new installation identifier: $newInstallationIdentifier")
+        cachedInstallationIdentifier = newInstallationIdentifier
+        return newInstallationIdentifier
     }
 }
