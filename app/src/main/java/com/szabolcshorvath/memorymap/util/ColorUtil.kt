e382ffed2e83764f8f1967af9677386e3b8e3ec5
Szabolcs Horvath <szaby0927@gmail.com>
2026-02-11 22:35:21 +0100

    Remove unused colorToHSV function from ColorUtil

--- app/src/main/java/com/szabolcshorvath/memorymap/util/ColorUtil.kt
+++ app/src/main/java/com/szabolcshorvath/memorymap/util/ColorUtil.kt
@@ -12,12 +12,6 @@ object ColorUtil {
         return Color.HSVToColor(floatArrayOf(normalizeHue(hue), 1.0f, 1.0f))
     }
 
-    fun colorToHSV(color: Int): FloatArray {
-        val hsv = FloatArray(3)
-        Color.colorToHSV(color, hsv)
-        return hsv
-    }
-
     fun textColorForBackground(backgroundColor: Int): Int {
         val darkness = 1 - (0.299 * Color.red(backgroundColor) +
                 0.587 * Color.green(backgroundColor) +
