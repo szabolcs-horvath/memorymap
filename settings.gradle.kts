394ba6f3e941109b8e62f87a48e0f663e1f3ee1d
Szabolcs Horvath <szaby0927@gmail.com>
2026-02-14 12:00:19 +0100

    Suppress lint warnings and unstable API usage notices
    - Add `@Suppress("UnstableApiUsage")` to `dependencyResolutionManagement` and `repositories` in `settings.gradle.kts`
    - Add `tools:ignore="DiscouragedApi,LockedOrientationActivity"` to `MainActivity` in `AndroidManifest.xml` to suppress orientation and API warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,9 @@ plugins {
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
