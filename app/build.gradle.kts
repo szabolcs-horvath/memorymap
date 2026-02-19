ef810b1744acdbb037793da282d867bde0a8f4c1
Szabolcs Horvath <szaby0927@gmail.com>
2026-02-19 20:47:05 +0100

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ kotlin {
             applicationId = "com.szabolcshorvath.memorymap"
             minSdk = 27
             targetSdk = 36
-            versionCode = 7
-            versionName = "Álmodj még cukibb keselyűkkel! <3"
+            versionCode = 8
+            versionName = "Nagyon szeretlek' <3"
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
