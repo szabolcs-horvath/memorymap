270a9e17fbd4e627a4ce9636e2405549e02cff0c
Szabolcs Horvath <szaby0927@gmail.com>
2026-03-26 19:31:53 +0100

    Bump application version code
    - Increment `versionCode` from 16 to 17 in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ kotlin {
             applicationId = "com.szabolcshorvath.memorymap"
             minSdk = 27
             targetSdk = 36
-            versionCode = 16
+            versionCode = 17
             versionName = "Büszke vagyok rád!"
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
