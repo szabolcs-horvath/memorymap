544fa24e01bebb2daeafa30987792c4b80807a24
Szabolcs Horvath <szaby0927@gmail.com>
2026-05-06 22:31:55 +0200

    Increment version code to 27
    - Update `versionCode` from 26 to 27 in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ kotlin {
             applicationId = "com.szabolcshorvath.memorymap"
             minSdk = 27
             targetSdk = 37
-            versionCode = 26
+            versionCode = 27
             versionName = "Imádlak :3"
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
